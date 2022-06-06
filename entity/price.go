bede0bfc595bb46f4f4229b8b998d7261a4fa648
Pornprasith Mahasith <jeurboy@gmail.com>
2022-06-06 17:11:08 +0700

    Change date format

--- entity/price.go
+++ entity/price.go
@@ -36,7 +36,7 @@ type IntString string
 type ChangeIntString string
 
 func (das DateString) ToDate() (t time.Time) {
-	layout := "02/01/2006"
+	layout := "02/01/06"
 	tp, err := time.Parse(layout, string(das))
 
 	if err != nil {
